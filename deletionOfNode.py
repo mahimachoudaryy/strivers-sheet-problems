cea285f4125fb77f1d36610abb91a8e9e5129117
mahimachoudaryy <mahimachoudary.tech@gmail.com>
2026-07-30 01:15:43 +0530

    Create deletionOfNode.py

---
+++ deletionOfNode.py
@@ -0,0 +1,10 @@
+# Definition of singly linked list:
+# class ListNode:
+#     def __init__(self, x=0, next=None):
+#         self.data = x
+#         self.next = next
+
+class Solution:
+    def deleteHead(self, head):
+        head=head.next
+        return head
